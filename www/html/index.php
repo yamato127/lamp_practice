7d83959165eef0f700a0dd2327e8b7c46551f6e7
Kazuto Yamamoto <kazuto.yamamoto12@gmail.com>
2021-07-15 23:40:08 +0900

    #17

--- www/html/index.php
+++ www/html/index.php
@@ -20,13 +20,16 @@ if(is_logined() === false){
   redirect_to(LOGIN_URL);
 }
 
+// 'selected_sort'のGET値を取得
+$selected_sort = get_get('selected_sort');
+
 // DBに接続（PDOを取得）
 $db = get_db_connect();
 // ログインユーザーのデータを取得
 $user = get_login_user($db);
 
 // 公開している商品データを取得
-$items = get_open_items($db);
+$items = get_open_items($db, $selected_sort);
 
 // ビューを読み込み
 include_once VIEW_PATH . 'index_view.php';
